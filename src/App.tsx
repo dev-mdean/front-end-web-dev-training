24521f21729336d48a76dbcafa1b5973724cc529
Matt Dean <matt.dean@indexarsolutions.com>
2024-03-05 14:55:55 -0500

    adds MaterialUI and sets up project for using it

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,11 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Box from "@mui/material/Box";
+import Typography from "@mui/material/Typography";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Box sx={{ backgroundColor: "grey.400", height: 1, width: 1 }}>
+      <Typography>Hello World</Typography>
+    </Box>
   );
 }
 
