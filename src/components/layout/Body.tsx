d70d03c30f09d06702a2fbfe8cd1ffa7d03295f8
Matt Dean <matt.dean@indexarsolutions.com>
2024-03-07 17:31:18 -0500

    shows and hides left panel with media query

--- src/components/layout/Body.tsx
+++ src/components/layout/Body.tsx
@@ -2,13 +2,26 @@ import Paper from '@mui/material/Paper'
 import Page from './Page'
 import LeftPanel from './LeftPanel'
 import Box from '@mui/material/Box'
+import useMediaQuery from '@mui/material/useMediaQuery'
+import { useTheme } from '@mui/material/styles'
 
 const Body = () => {
+  const theme = useTheme()
+  const showMobileLayout = useMediaQuery(theme.breakpoints.down('lg'))
+
   return (
-    <Box sx={{ display: 'flex', height: 1 }}>
-      <Box>
-        <LeftPanel />
-      </Box>
+    <Box
+      sx={{
+        display: 'flex',
+        flexDirection: showMobileLayout ? 'column' : 'row',
+        height: 1,
+      }}
+    >
+      {!showMobileLayout && (
+        <Box>
+          <LeftPanel />
+        </Box>
+      )}
       <Paper
         elevation={0}
         sx={{
