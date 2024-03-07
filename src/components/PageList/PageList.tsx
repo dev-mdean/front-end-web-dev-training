78f98f30fda14502c8829c87481c43cf8a38873e
Matt Dean <matt.dean@indexarsolutions.com>
2024-03-07 17:16:18 -0500

    adds page list item component

--- src/components/PageList/PageList.tsx
+++ src/components/PageList/PageList.tsx
@@ -1,12 +1,28 @@
 import List from '@mui/material/List'
 import PageListItem from './PageListItem'
+import { useCallback, useState } from 'react'
+
+const ITEMS: string[] = ['Item 1', 'Item 2', 'Item 3']
 
 const PageList = () => {
+  const [selectedItem, setSelectedItem] = useState<string>()
+
+  const handleClick = useCallback(
+    (event: React.SyntheticEvent, value: string) => {
+      setSelectedItem(value)
+    },
+    []
+  )
+
   return (
     <List disablePadding>
-      <PageListItem text='Item 1' />
-      <PageListItem text='Item 2' />
-      <PageListItem text='Item 3' />
+      {ITEMS.map((item) => (
+        <PageListItem
+          onClick={handleClick}
+          selected={item === selectedItem}
+          value={item}
+        />
+      ))}
     </List>
   )
 }
--- src/components/PageList/PageListItem.tsx
+++ src/components/PageList/PageListItem.tsx
@@ -1,16 +1,37 @@
 import ListItemButton from '@mui/material/ListItemButton'
 import ListItemText from '@mui/material/ListItemText'
 import ListItem from '@mui/material/ListItem'
+import ListItemIcon from '@mui/material/ListItemIcon'
+import KeyboardArrowDownIcon from '@mui/icons-material/KeyboardArrowDown'
+import KeyboardArrowRightIcon from '@mui/icons-material/KeyboardArrowRight'
+import { useCallback } from 'react'
 
 type PageListItemProps = {
-  text: string
+  onClick?: (event: React.SyntheticEvent, value: string) => any
+  selected: boolean
+  value: string
 }
 
-const PageListItem = ({ text }: PageListItemProps) => {
+const PageListItem = ({ onClick, selected, value }: PageListItemProps) => {
+  const handleClick = useCallback(
+    (event: React.SyntheticEvent) => {
+      event.stopPropagation()
+      onClick?.(event, value)
+    },
+    [onClick, value]
+  )
+
   return (
     <ListItem disablePadding>
-      <ListItemButton>
-        <ListItemText primary={text} />
+      <ListItemButton onClick={handleClick} selected={selected}>
+        <ListItemIcon>
+          {selected ? (
+            <KeyboardArrowDownIcon fontSize='small' />
+          ) : (
+            <KeyboardArrowRightIcon color='primary' fontSize='small' />
+          )}
+        </ListItemIcon>
+        <ListItemText primary={value} />
       </ListItemButton>
     </ListItem>
   )
