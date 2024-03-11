ac18573db6e69aff55219ac6a15ead2aa8b6ecc5
Matt Dean <matt.dean@indexarsolutions.com>
2024-03-11 12:49:00 -0400

    opens non-relative urls in a separate tab

--- src/components/layout/Page.tsx
+++ src/components/layout/Page.tsx
@@ -1,6 +1,32 @@
-import { useEffect, useState } from 'react'
+import React, { useEffect, useMemo, useState } from 'react'
 import { MuiMarkdown } from 'mui-markdown'
 import mdPath from '../../md-files/training/training-notes.md'
+import { Link as RouterLink } from 'react-router-dom'
+import { Link } from '@mui/material'
+
+const MdLink = ({
+  href,
+  children,
+}: React.DetailedHTMLProps<
+  React.AnchorHTMLAttributes<HTMLAnchorElement>,
+  HTMLAnchorElement
+>): React.ReactElement<any, any> => {
+  let to = href ?? '/'
+
+  const target = useMemo(() => {
+    const nonRelativeUrls = ['http', 'https']
+
+    if (nonRelativeUrls.some((value) => href?.startsWith(value)))
+      return '_blank'
+    return '_self'
+  }, [href])
+
+  return (
+    <Link component={RouterLink} target={target} to={to}>
+      {children}
+    </Link>
+  )
+}
 
 const Page = () => {
   const [md, setMd] = useState('')
@@ -15,7 +41,17 @@ const Page = () => {
     })
   }, [])
 
-  return <MuiMarkdown>{md}</MuiMarkdown>
+  return (
+    <MuiMarkdown
+      options={{
+        overrides: {
+          a: MdLink,
+        },
+      }}
+    >
+      {md}
+    </MuiMarkdown>
+  )
 }
 
 export default Page
