5da45b2f0f9f2ada290645a398cdd8732728d547
jacob.page@indexarsolutions.com <jacob.page@indexarsolutions.com>
2024-03-25 16:39:30 -0400

    PT 4 to previous commit

--- src/index.tsx
+++ src/index.tsx
@@ -4,7 +4,15 @@ import App from './components/App'
 import './index.scss'
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
-document.body.style.overflow = 'hidden'
+document.body.classList.add('no-scroll')
+
+const preventDefault = (e: any) => e.preventDefault()
+// When rendering our container
+window.addEventListener('touchmove', preventDefault, {
+  passive: false,
+})
+// Remember to clean up when removing it
+window.removeEventListener('touchmove', preventDefault)
 
 root.render(
   <React.StrictMode>
